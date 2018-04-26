b11779468861a339476448c695ed9c811206e87c
mononym <mononym@users.noreply.github.com>
2018-04-26 13:29:48 -0700

    add docs; add specs; dedupe logic

--- lib/uber_multi.ex
+++ lib/uber_multi.ex
@@ -1,14 +1,61 @@
 defmodule UberMulti do
   @moduledoc """
-  Documentation for UberMulti.
+  UberMulti is a simple wrapper/helper for 'Multi.run/3' and 'Multi.run/5'.
+
+  There are two differences when using UberMulti compared to the normal Multi methods. The first is the addition of the 'keys' argument. The keys provided in this argument are used in two ways. First, each key is used to try and extract a result from the list of previous changes in the execution of the multi. If a result is not found, the key is instead used as-is as the parameter. In this way you can mix and match between providing your own arguments and automatically fetching them from previous multi results.
+
+  The second difference is in the function that will be run by the multi. Given that the changes list is being preprocessed into a list of parameters to pass to the function, it will not need to take in the list of changes as normal, but simply take in the list of parameters it expects to perform its function.
+
+  So an UberMulti which uses the following function:
+  '''
+  def uber_cool_things(thing1, thing2) do
+    {:ok, thing1 + thing2}
+  end
+  '''
+
+  Might look like:
+  '''
+  UberMulti.run(multi, :add_things, [:get_thing1, :get_thing2], &uber_cool_things/2)
+  '''
+
+  Obviously this is very contrived, but shows how you can easily combine non-multi functions together without having to wrap all the calls yourself and manually extract the results for the next one in the chain.
   """
 
   alias Ecto.Multi
+
+  @typedoc "An '%Ecto.Multi{}' struct. See 'Ecto.Multi' for detailed specs."
+  @type multi :: struct()
+
+  @typedoc "A name to tag the multi function with. Must be unique within a single multi chain."
+  @type name :: term()
+
+  @typedoc "One or more keys, with a single key optionally being in a list."
+  @type keys :: key | [key]
+
+  @typedoc "The key to extract the parameters from using the changes map, or a value to pass directly to the callback."
+  @type key :: name | term()
+
+  @typedoc "A callback function to call with the parameters extracted from the changes map."
+  @type run :: function()
+
+  @typedoc "A module to call a callback function on."
+  @type m :: atom()
+
+  @typedoc "A callback function to call."
+  @type f :: atom()
+
+  @typedoc "An optionally empty list of arguments to append to the extracted parameters before calling the callback function."
+  @type args :: [] | [arg]
+
+  @typedoc "The argument."
+  @type arg :: term()
 
   @doc """
   Adds a function to run as part of the multi.
 
   The function should return either '{:ok, value}' or '{:error, value}' and will receive whatever arguments are specified by the passed in keys in order, rather than the changes so far as with a normal 'Ecto.Multi.run/3' call.
+
+  If one of the provided keys cannot be found in the list of changes, it is instead inserted as-is as one of the parameters to the callback function. In this way you can mix and match between providing your own arguments and automatically fetching them from previous multi results.
 
   ## Examples
 
@@ -17,16 +64,18 @@ defmodule UberMulti do
       end)
       #Ecto.Multi{}
 
+      Multi.new()
+      |> Multi.run(:list_stars, fn(_) -> Astronomy.list_stars("Milky Way") end)
+      |> UberMulti.run(:reverse, [:list_stars], &Enum.reverse/1)
+      |> UberMulti.run(:cannon, [:reverse], &Astronomy.classify_stars/1)
+      |> Repo.transaction()
   """
+  @spec run(multi, name, keys, run) :: multi
   def run(multi, name, keys, run) do
     Multi.run(multi, name, fn changes ->
-      args =
-        Enum.reduce(keys, [], fn key, list ->
-          [Map.get(changes, key)] ++ list
-        end)
-        |> Enum.reverse()
+      extracted_args = extract_args(keys, changes)
 
-      apply(run, args)
+      apply(run, extracted_args)
     end)
   end
 
@@ -41,15 +90,23 @@ defmodule UberMulti do
       #Ecto.Multi{}
 
   """
+  @spec run(multi, name, keys, m, f, args) :: multi
   def run(multi, name, keys, module, function, args \\ []) do
     Multi.run(multi, name, fn changes ->
-      extracted_args =
-        Enum.reduce(keys, [], fn key, list ->
-          [Map.get(changes, key)] ++ list
-        end)
-        |> Enum.reverse()
+      extracted_args = extract_args(keys, changes)
 
       apply(module, function, extracted_args ++ args)
+    end)
+  end
+
+  defp extract_args(keys, changes) do
+    keys = List.wrap(keys) |> Enum.reverse()
+    Enum.reduce(keys, [], fn key, args ->
+      if Map.has_key?(changes, key) do
+        [Map.get(changes, key) | args]
+      else
+        [key | args]
+      end
     end)
   end
 end
